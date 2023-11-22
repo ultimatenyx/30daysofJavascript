6369c42dabda594a4d458e0f6bd79621d783a8cd
Arcanyx <nikhilchaughule@gmail.com>
2023-11-22 14:18:14 +0530

    Day 17

---
+++ challenge17 - Write a JavaScript function named filterOutOdd that uses the filter() method to filter out odd numbers from an array.js
@@ -0,0 +1,17 @@
+// **Challenge 17**
+// Problem Description:
+// Write a JavaScript function named filterOutOdd that uses the filter() method to filter out odd numbers from an array.
+// Inputs:
+// An array of numbers.
+// Outputs:
+// An array of numbers without odd numbers.
+// Hints:
+// Use the filter() method to create a new array with all elements that pass the test implemented by the provided function.
+// Solution Approach:
+// Use the filter() method and provide a callback that checks if the number is even.
+
+let arr = [1,2,3,4,5,6,7,8];
+function filterOutOdd(a){
+    return a.filter((num) => num%2==0);
+}
+console.log(filterOutOdd(arr));
